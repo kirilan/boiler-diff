ded7ab69a9d71842b0c11166131ab9be266e2eb4
kirilan <kirilan@abv.bg>
2015-01-19 11:26:27 +0200

    removed reactive-tables

--- client/views/creq/creq.js
+++ client/views/creq/creq.js
@@ -1,37 +1,3 @@
 Template.creq.rendered = function() {
 
 };
-
-Template.creq.helpers({
-  settings: function () {
-    return {
-      collection: SystemListCREQ,
-      rowsPerPage: 10,
-      showFilter: true,
-      fields: ['DESCRIPTION',
-      'HS_FLAG_DESCRIPTION',
-      'SYSTEM_TYPE_DESC',
-      'SYSTEM_PURPOSE',
-      'VIRTUAL_FLAG',
-      'VALIDATED',
-      'OPERATING_SYSTEM',
-      'SERVER_SN',
-      'HARDWARE_STATUS',
-      'PRODUCT_TYPE',
-      'CLASSIFICATION',
-      'DATA_CENTER_NAME',
-      'DECOMMISSIONED',
-      'DECOMMISSION_DATE'],
-      rowClass: function(item) {
-        var qnt = item.DECOMMISSIONED;
-
-        switch (qnt) {
-          case 'Y':
-            return 'danger';
-              default:
-                  return ''
-                }
-              }
-    };
-  }
-});
