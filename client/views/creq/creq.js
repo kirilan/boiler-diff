4972d0dd7b7cb26291e2f3f7149a745531fe54dc
Kiril Dimitrov <kirilan@abv.bg>
2015-01-20 21:52:35 +0200

    creq view selectors reworked

--- client/views/creq/creq.js
+++ client/views/creq/creq.js
@@ -2,13 +2,28 @@ Template.creq.rendered = function() {
 
 };
 
-Template.creq.events({
+Template.creq_main.events({
   'click tr': function (event) {
     //get current row and set session values
     var dataTable = $(event.target).closest('table').DataTable();
     var rowData = dataTable.row(event.currentTarget).data();
     Session.set('selected_system',rowData.DESCRIPTION);
+    Session.set('selected_main',rowData.DESCRIPTION);
     Session.set('selected_system_type',rowData.SYSTEM_TYPE_DESC);
+  },
+  'click .expand': function () {
+    Session.set('selected_main',undefined)
+    console.log('1')
+  }
+});
+
+Template.creq_main.helpers({
+  selector: function () { //return selector to limit apps view
+    var selected = Session.get('selected_main');
+    if (selected) {
+      return {DESCRIPTION: selected};
+    }
+
   }
 });
 
@@ -20,15 +35,21 @@ Template.creq_apps.events({
     var cellData = dataTable.cells(event.currentTarget).data();
     var cellIndexes = dataTable.cells(event.currentTarget).indexes();
 
-  //  console.log(cellData[0]);
-  //  console.log(cellIndexes[0].column);
-
     if ( (cellIndexes[0].column == 1) && (typeof cellData[0]!= "undefined") ) {
       console.log(cellData[0]);
+      Session.set('selected_main',cellData[0]);
+      Session.set('selected_system',rowData.DESCRIPTION);
 
     }
 
-  }
+  },
+  'click tr': function (event) {
+    //get current row and set session values
+    var dataTable = $(event.target).closest('table').DataTable();
+    var rowData = dataTable.row(event.currentTarget).data();
+    Session.set('selected_system',rowData.DESCRIPTION);
+    Session.set('selected_system_type',rowData.SYSTEM_TYPE_DESC);
+  },
 });
 
 Template.creq_apps.helpers({
